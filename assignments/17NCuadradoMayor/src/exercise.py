4aa289fac49bf1a74594e7a214fab9ad9a7e90cf
A01748505 <89160214+A01748505@users.noreply.github.com>
2021-09-06 20:02:41 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,7 +1,10 @@
-
-
 def main():
     #Escribe tu código debajo de esta línea
+    num=int(input("Escribe un numero : "))
+    contador=1
+    while (contador**2)<=num:
+        contador=contador+1
+    print (contador)
     pass
 
 if __name__=='__main__':
