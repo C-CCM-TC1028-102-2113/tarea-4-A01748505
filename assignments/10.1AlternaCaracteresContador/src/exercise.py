cd6af7e26acd40c4373e5bbb224e4ae071a4519d
A01748505 <89160214+A01748505@users.noreply.github.com>
2021-09-06 19:48:28 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input())
+    contador=0
+    while n>=0:
+        contador=contador+1
+        if contador % 2 == 0:
+            print(contador, "-%")
+        else:
+            print(contador, "-#")
+        if contador>=n:
+            break
+    contador=contador+1
     pass
 
 if __name__=='__main__':   
